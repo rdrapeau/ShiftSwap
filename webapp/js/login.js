87dac66de6700c89d4d5b3d32934fa0c1d46672a
Ryan Drapeau <drapeau@cs.washington.edu>
2014-04-10 23:16:29 -0700

    Webapp / phone app initial

---
+++ webapp/js/login.js
@@ -0,0 +1,29 @@
+(function() {
+    "use strict";
+
+    $(document).ready(function() {
+        $("#alreadyRegistered").click(alreadyRegistered);
+        $("#register").click(register)
+    });
+
+    // Called when the user clicks the alreadyRegistered text
+    function alreadyRegistered() {
+        $("#alreadyRegistered").fadeOut("fast");
+        $("#registerForm").slideUp("slow", function() {
+            $("#loginForm").slideDown("slow");
+        });
+    }
+
+    // Called when the user clicks the register button
+    function register() {
+        var email = $("#registerEmail").val();
+        var company = $("#registerCompany").val();
+        var password1 = $("#registerPassword").val();
+        var password2 = $("#registerPassword2").val();
+
+        // On Success Bring up the login forms
+        $("#welcomeText").fadeIn("slow");
+        alreadyRegistered();
+    }
+
+})();
