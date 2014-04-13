d164c1a9972eec6920925c5205260b4b87a00156
setareh94 <setarehlotfi94@hotmailcom>
2014-04-12 17:31:38 -0700

    revision

--- webapp/js/dashboard.js
+++ webapp/js/dashboard.js
@@ -6,20 +6,55 @@
     $(document).ready(function() {
         $("#alreadyRegistered").click(alreadyRegistered);
         $("#remember").hide();
-        $("#register").click(register);
+        $("#register").click(register, showDash);
         $("#login").click(login);
+        $("#sign-in").click(sendgrid);
+
     });
 
     // Called when the user clicks the alreadyRegistered text
     function alreadyRegistered() {
         $("#alreadyRegistered").fadeOut("fast");
         $("#registerForm").slideUp("slow", function() {
-            $("#loginForm").slideDown("slow");
+        $("#loginForm").slideDown("slow");
         $("#remember").show();    
         });
     }
+    //sendgrid function sending data
 
-    // Called when the user clicks the register button
+    function sendgrid(){
+        var sendgrid_username = 'setarehlotfi94@hotmaitl.com'
+        var sendgrid_password = 'Classof2017'
+        var email = employees_Email
+         $.get(server + '/manager/employees_Email', {'email' : email}, function(data) {
+            if(data.response == 'OK') {
+                $.get(server + '/manager/employees_Email', {'email' : email}, function(data) {
+                    if(data.response == 'OK') {
+                        showDash(data);
+                    } else {
+                        alert(data);
+                    }
+                });
+            } else {
+                alert(data)
+            }
+        });
+            //subject : 'IMPORTANT: Your Login Information for ShiftSwape'
+        var sendgrid = require('sendgrid')(sendgrid_username, sendgrid_password);
+        var email = new sendgrid.Email();
+        
+        email.addTo(to);
+        email.setFrom('info@ShiftSwape.com');
+        email.setsubject('Your Login Infomration with SendGrid');
+        email.setText('Here is your login information for ShiftSwape: %d Please download our mobile app from Android or App Store and Start Swapping your shifts when you needed too!', userId)
+
+        sendgrid.send(email, function(erro, json){
+            if (err) {return console.error(err);}
+            console.log(json);
+        });
+    
+    }
+
     function register() {
         var email = $("#registerEmail").val();
         var password1 = $("#registerPassword").val();
@@ -38,6 +73,8 @@
             }
         });
     }
+
+
 
     function login() {
         var email = $("#loginEmail").val();
